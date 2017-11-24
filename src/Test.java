07835d89fb97cb29d664b7f6d2cc05d07aa89441
Ninnix <nicolo.deva@gmail.com>
2017-11-24 18:19:52 +0100

    aggiunto metodo check

--- src/Test.java
+++ src/Test.java
@@ -13,11 +13,26 @@ public class Test {
             array[i] = random;
         }
         System.out.println(Arrays.toString(array));
-
-        MergeSort.mergeSort(array,0,array.length -1);
+        System.out.println(check(array)); //stampa false
+        MergeSort.mergeSort(array,0,array.length-1);
         System.out.println(Arrays.toString(array));
+        System.out.println(check(array)); //stampa true
+    }
 
-
+    /**
+     * verifica sequenzialmente se l'array e' correttamente ordinato
+     * @param array
+     * @return
+     */
+    static boolean check(int[] array) {
+        boolean ans = true;
+        for (int i = 0; i < array.length-1; i++){
+            if (array[i] < array[i + 1]) {
+                ans = true;
+            }
+            else ans = false;
+        }
+        return ans;
     }
 
 }
