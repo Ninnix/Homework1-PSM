716cd6609102746618c1361989bd80c199c6c511
D'Evangelista Nicolo <nicolo.deva@gmail.com>
2017-11-24 15:56:44 +0100

    Add files via upload

---
+++ src/MergeSort.java
@@ -0,0 +1,55 @@
+public class MergeSort {
+
+    /**
+     *
+     * @param array
+     * @param low
+     * @param high
+     */
+    static void mergeSort(int[] array, int low, int high) {
+        if (low < high) {
+            int middle = (low + high)/2;
+            mergeSort(array, low, middle);
+            mergeSort(array, middle + 1, high);
+            merge(array, low, middle, high);
+        }
+    }
+
+    /**
+     *
+     * @param array
+     * @param low
+     * @param middle
+     * @param high
+     */
+    static void merge (int[] array, int low, int middle, int high) {
+
+        int[] aux = new int[array.length];
+        for(int i = low; i <= high; i++){
+            aux[i] = array[i];
+        }
+
+        int left = low;
+        int right = middle + 1;
+        int current = low;
+
+
+        while (left <= middle && right <= high) {
+            if(aux[left] <= aux[right]){
+                array[current] = aux[left];
+                left++;
+
+            }else{
+                array[current] = aux[right];
+                right++;
+            }
+            current ++;
+        }
+
+        int remaining = middle - left;
+        for (int i = 0; i <= remaining; i++) {
+            array[current+i] = aux[left+ i];
+        }
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int lenght = 1000;
+        int[] array = new int[lenght];
+
+        for (int i = 0; i < lenght; i++) {
+            Random rn = new Random();
+            int random = rn.nextInt(100) ;
+            array[i] = random;
+        }
+        System.out.println(Arrays.toString(array));
+
+        MergeSort.mergeSort(array,0,array.length -1);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
