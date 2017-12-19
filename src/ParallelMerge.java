08c72d1052b997b03c86b956b35c16e4cacfa446
Ninnix <nicolo.deva@gmail.com>
2017-12-19 14:14:38 +0100

    Riaggiunte classi per i test di velocita'

---
+++ src/ParallelMerge.java
@@ -0,0 +1,78 @@
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class ParallelMerge extends RecursiveAction {
+
+    static final ForkJoinPool fjPool = new ForkJoinPool();
+    private int[] array;
+    private int[] aux;
+    private int sxLow;
+    private int sxHigh;
+    private int dxLow;
+    private int dxHigh;
+    private int auxLow;
+    private int auxHigh;
+    private int SEQUENTIAL_CUTOFF = 1024;
+
+    ParallelMerge(int[] arr,int sxL,int sxH,int dxL, int dxH, int[] aux, int auxL, int auxH) {
+        array = arr;
+        this.aux = aux;
+        sxLow = sxL;
+        sxHigh = sxH;
+        dxLow = dxL;
+        dxHigh = dxH;
+        auxLow = auxL;
+        auxHigh = auxH;
+    }
+
+    @Override
+    protected void compute() {
+        int lenSx = sxHigh - sxLow;
+        int lenDx = dxHigh - dxLow;
+        if (lenSx < lenDx) { //scambio gli indici in modo da avere sempre array di sinistra piu' grande
+            int appoggio;
+
+            appoggio = sxLow;
+            sxLow = dxLow;
+            dxLow = appoggio;
+
+            appoggio = sxHigh;
+            sxHigh = dxHigh;
+            dxHigh = appoggio;
+
+            appoggio = lenSx;
+            lenSx = lenDx;
+            lenDx = appoggio;
+        }
+        /**
+        if (lenSx == 1 && lenDx == 1) {
+            if (aux[sxLow] <= aux[dxLow]) {
+                array[auxLow] = aux[sxLow];
+                array[auxLow+1] = aux[dxLow];
+            } else {
+                array[auxLow] = aux[dxLow];
+                array[auxLow+1] = aux[sxLow];
+            }
+            return;
+        }
+        if (lenSx == 1 && lenDx == 0) {
+            array[auxLow] = aux[sxLow];
+            return;
+        } **/
+        if (lenSx < SEQUENTIAL_CUTOFF) {
+            Sorting.serialMerge2(array,sxLow,sxHigh,dxLow,dxHigh,aux,auxLow,auxHigh);
+            return;
+        }
+        else {
+            int sxMed = (sxHigh + sxLow) / 2; //mediano sottoarray piu' grande
+            int dxInd = Utils.binarySearch(aux[sxMed], aux, dxLow, dxHigh); //posizione mediano sottoarray più piccolo
+            int auxInd = (sxMed - sxLow) + (dxInd - dxLow); //numero elementi (indice) piu' piccoli del sxMed
+            ParallelMerge left = new ParallelMerge(array, sxLow, sxMed, dxLow, dxInd, aux, auxLow,auxLow + auxInd);
+            ParallelMerge right = new ParallelMerge(array, sxMed, sxHigh, dxInd, dxHigh, aux, auxLow + auxInd, auxHigh);
+            left.fork();
+            right.compute();
+            left.join();
+        }
+    }
+}
